ed515f7b141f68dbd39b95d464891b04b65be09e
ricE06 <ezzhaneri06@gmail.com>
2024-11-24 00:12:50 -0500

    support for text file encoding

--- backend/puzzle_app/puzzles/puzzle_core/constructors.py
+++ backend/puzzle_app/puzzles/puzzle_core/constructors.py
@@ -3,4 +3,232 @@ This file implements helper functions to:
     create Puzzle and Solution instances from text files
     create Puzzle and Solution instances from JSON files
     get a Puzzle instance from the id in the database
+    create Solution instances
 """
+
+from typing import Any
+from abc import ABC, abstractmethod
+from puzzles.puzzle_core import Puzzle
+
+TEXT_COMMENT_SYMBOL = '%'
+WHITESPACE = ' \t'
+BRACES = '()'
+LEFT_BRACE = '(' #)
+RIGHT_BRACE = ')' 
+
+
+class PuzzleConstructor():
+    """
+    Contains helper functions to load puzzles.
+    """
+    
+    @staticmethod
+    def load_txt(data: str) -> Puzzle:
+        """
+        Loads a puzzle from a raw text file.
+        """
+        raise NotImplementedError
+
+    @staticmethod
+    def load_json(data: dict) -> Puzzle:
+        """
+        Loads a puzzle from a raw json file.
+        """
+        raise NotImplementedError
+
+class TextParser():
+
+    class TextParsingError(Exception):
+        pass
+
+    class Parser(ABC):
+
+        def parse(self, tokens: str|list) -> Any:
+            print(self.__class__.__name__)
+            if isinstance(tokens, list):
+                if len(tokens) == 0:
+                    return []
+                return list(map(self.parse_one, tokens))
+            return self.parse_one(tokens)
+
+        @abstractmethod
+        def parse_one(self, token: str|list) -> Any:
+            raise NotImplementedError('Subclasses of Parser must implement `parse_one`!')
+
+    class SelfParser(Parser):
+
+        def parse_one(self, token: str|list[str]) -> str|list[str]:
+            return token
+
+    class BaseParser(Parser):
+
+        def number_or_other(self, token: str|list[str]) -> Any:
+            if isinstance(token, list): return token
+            try: return int(token)
+            except ValueError: pass
+            try: return float(token)
+            except ValueError: pass
+            return token
+
+        def parse_one(self, token: str|list[str]) -> Any:
+            return self.number_or_other(token)
+
+    class StateParser(Parser):
+        DELIMITER = '-'
+        EMPTY_TOKEN = '_'
+
+        def parse_one(self, token: str|list) -> Any:
+            if isinstance(token, list):
+                raise TextParser.TextParsingError('States cannot be nested!')
+            return tuple(token for token in token.split(self.DELIMITER) 
+                         if token != self.EMPTY_TOKEN)
+
+    class ListParser(Parser):
+
+        def __init__(self, sub_parser):
+            self.sub_parser = sub_parser
+
+        def parse_one(self, token: str|list) -> Any:
+            return self.sub_parser.parse(token)
+
+    class DictParser(SelfParser):
+        IMPLICIT_KEYS = {'-': 'type'}
+
+        def __init__(self, sub_parser=None, custom_parsers=None):
+            self.sub_parser = TextParser.SelfParser() if sub_parser is None else sub_parser
+            self.custom_parsers = {} if custom_parsers is None else custom_parsers
+            self.out_dict = {}
+
+        def parse(self, tokens: str|list) -> Any:
+            super().parse(tokens)
+            copy = self.out_dict # careful, not a shallow copy
+            self.out_dict = {}
+            return copy
+
+        def parse_one(self, token: str|list) -> Any:
+            print(token)
+            if isinstance(token, str):
+                if token[0] not in self.IMPLICIT_KEYS:
+                    raise TextParser.TextParsingError(f'No implicit key found for {token}!')
+                key = self.IMPLICIT_KEYS[token[0]]
+                val = token[1:]
+            else:
+                if len(token) < 2:
+                    raise TextParser.TextParsingError(
+                            f'Assignment in DictParser requires a key and value!')
+                key, *val = token
+                if len(val) == 1: val = val[0]
+                parser = self.custom_parsers.get(key, self.sub_parser)
+                val = parser.parse(val)
+            self.out_dict[key] = val
+        
+
+    @staticmethod
+    def raw_tokens_txt(inp_txt: str) -> list[str]:
+        """
+        Tokens a text file, stripping whitespace and ignoring the
+        comment delimiter.
+        """
+        def tokenize_line(line: str, out: list[str]) -> None:
+            """Helper to tokenize a single line."""
+            token = ""
+
+            def end_token(token: str) -> str:
+                if token:
+                    out.append(token)
+                    token = ""
+                return token
+
+            for char in line:
+                if char in WHITESPACE:
+                    # stop current token or ignore if no token exists
+                    token = end_token(token)
+                elif char in BRACES:
+                    token = end_token(token)
+                    out.append(char) # individual brace is a token, spaces or not
+                elif char in TEXT_COMMENT_SYMBOL:
+                    token = end_token(token)
+                    break # ignore rest of the line
+                else:
+                    token += char
+
+            end_token(token)
+            return None # we mutate out in-place
+
+        out = []
+        for line in inp_txt.splitlines():
+            tokenize_line(line, out)
+
+        return out
+
+    @classmethod
+    def nested_tokens_txt(cls, inp_txt: str) -> list[list|str]:
+        """
+        Takes the tokens from `raw_token_txt` and nests them, such that
+        any tokens that are surrounded by matching braces 
+        are in a nested list.
+        """
+        tokens = cls.raw_tokens_txt(inp_txt)
+        out = []
+        left_idxs = []
+        for idx, token in enumerate(tokens):
+            if token == LEFT_BRACE:
+                left_idxs.append(len(out)) # keep track of when nest starts
+            elif token == RIGHT_BRACE:
+                # put everything between left and right braces 
+                # into its own list entry
+                if not left_idxs:
+                    raise TextParser.TextParsingError(f'Unmatched close brace at token #{idx}!')
+                # append, not extend, as this is one entry
+                last_idx = left_idxs.pop()
+                new_token = out[last_idx:]
+                out = out[:last_idx]
+                out.append(new_token)
+            else:
+                out.append(token)
+        return out
+
+    @classmethod
+    def parse_txt(cls, inp_txt: str) -> list[dict[str, Any]]:
+        """
+        Parses a text file. Returns a list of puzzle dictionaries.
+        """
+        tokens = cls.nested_tokens_txt(inp_txt)
+        return [cls._parse_puzzle(puzz) for puzz in tokens]
+
+    SUBPUZZLE_FLAGS = {'--rules': {'parser': ListParser(DictParser(BaseParser())), 'name': 'rules'},
+                       '--grid': {'parser': DictParser(BaseParser()), 'name': 'grid'},
+                       '--vertices': {'parser': DictParser(BaseParser(), 
+                                                           custom_parsers={'data': StateParser()}), 
+                                      'name': 'vertices'}}
+
+    @classmethod
+    def _parse_puzzle(cls, tokens: list[list|str]) -> dict[str, Any]:
+        """
+        Parses part of the tokens list that describes a puzzle dictionary.
+        Returns:
+            ptr: the pointer the parsing ended at and for the next parser to continue with
+            out: a dictionary describing the Puzzle object (but not a Puzzle object), this
+                should be passed into the PuzzleConstructor
+        """
+        out = {}
+        ptr = 0
+        print(tokens)
+        while ptr < len(tokens) // 2:
+            flag, expr = tokens[2*ptr], tokens[2*ptr+1]
+            if not isinstance(flag, str):
+                raise cls.TextParsingError(f"Flag {flag} is not a string!")
+            if (flag := flag.lower()) not in cls.SUBPUZZLE_FLAGS:
+                raise cls.TextParsingError(f"Subpuzzle flag {flag} not recognized!")
+            # parse and add to dict
+            print(f'{expr=}')
+            puzzle_attr = cls.SUBPUZZLE_FLAGS[flag]['parser'].parse(expr)
+            out[cls.SUBPUZZLE_FLAGS[flag]['name']] = puzzle_attr
+            # move to next token
+            ptr += 1
+            
+        return out
+
+
+
+            
--- backend/puzzle_app/puzzles/puzzle_core/test_puzzle_logic.py
+++ backend/puzzle_app/puzzles/puzzle_core/test_puzzle_logic.py
@@ -11,7 +11,15 @@ Tests that should go here include:
 """
 import pytest
 from typing import cast
-from puzzles.puzzle_core import Puzzle, RectGrid, BuiltinSymbols
+from puzzles.puzzle_core import Puzzle, RectGrid, BuiltinSymbols, TextParser
+
+PATH_TO_TEST_PUZZLES = 'puzzles/puzzle_core/test_puzzles/'
+
+def load_from_file(base_file_name):
+    file_name = PATH_TO_TEST_PUZZLES + base_file_name
+    with open(file_name, 'r') as file:
+        return file.read()
+
 
 class Test_Puzzles_Debug():
     """
@@ -63,7 +71,191 @@ class Test_Puzzles_Debug():
         print(exp)
         assert str(puz) == exp
 
+class Test_Text_Tokenizer():
+    """
+    Tests the tokenizer to parse an input text file correctly,
+    all the way to returning the correct dictionary 
+    (the equivalent of doing an easy JSON load).
+    """
+
+    def test_raw_tokens(self):
+        simple = "(3) \n(t urt\t (le))"
+        exp = ['(', '3', ')', '(', 't', 'urt', '(', 'le', ')', ')', ]
+        output = TextParser.raw_tokens_txt(simple)
+        assert exp == output
+
+    def test_raw_tokens_2(self):
+        simple = "(1 2 3\n 4 5 6\n 7 8 9)"
+        exp = ['(', '1', '2', '3', '4', '5', '6', '7', '8', '9', ')']
+        output = TextParser.raw_tokens_txt(simple)
+        assert exp == output
+
+    def test_raw_tokens_comments(self):
+        simple = "(3) \t(t% urt\n (le))"
+        exp = ['(', '3', ')', '(', 't', '(', 'le', ')', ')', ]
+        output = TextParser.raw_tokens_txt(simple)
+        assert exp == output
+
+    def test_empty_tokens(self):
+        empty = "%%%%%%%%%"
+        exp = []
+        output = TextParser.raw_tokens_txt(empty)
+        assert exp == output
+
+    def test_nested_tokens(self):
+        simple = "(3) \n(t urt\t (le))"
+        exp = [['3'], ['t', 'urt', ['le'], ], ]
+        output = TextParser.nested_tokens_txt(simple)
+        assert exp == output
+
+    @pytest.fixture
+    def self_parser(self):
+        self.parser = TextParser.SelfParser()
+
+    def test_self_parser_1(self, self_parser):
+        token = ['3']
+        exp = ['3']
+        out = self.parser.parse(token)
+        assert exp == out
+
+    def test_self_parser_2(self, self_parser):
+        token = ['3', '4']
+        exp = ['3', '4']
+        out = self.parser.parse(token)
+        assert exp == out
+
+    def test_self_parser_3(self, self_parser):
+        token = [['3', '4']]
+        exp = [['3', '4']]
+        out = self.parser.parse(token)
+        assert exp == out
+
+    @pytest.fixture
+    def state_parser(self):
+        self.parser = TextParser.StateParser()
+
+    def test_state_parser_1(self, state_parser):
+        token = 'WH-BK-4'
+        exp = ('WH', 'BK', '4')
+        out = self.parser.parse(token)
+        assert exp == out
+    
+    def test_state_parser_2(self, state_parser):
+        token = '_'
+        exp = tuple()
+        out = self.parser.parse(token)
+        assert exp == out
+    
+    def test_state_parser_3(self, state_parser):
+        token = ['WH-BK-4', '_', '1']
+        exp = [('WH', 'BK', '4'), tuple(), ('1',)]
+        out = self.parser.parse(token)
+        assert exp == out
+
+    @pytest.fixture
+    def list_parser(self):
+        sub_parser = TextParser.StateParser()
+        self.parser = TextParser.ListParser(sub_parser)
+
+    def test_list_parser_1(self, list_parser):
+        token = []
+        exp = []
+        out = self.parser.parse(token)
+        assert exp == out
+    
+    def test_list_parser_2(self, list_parser):
+        token = ['WH-BK-4', '_', '1']
+        exp = [('WH', 'BK', '4'), tuple(), ('1',)]
+        out = self.parser.parse(token)
+        assert exp == out
+
+    def test_list_parser_3(self, list_parser):
+        token = ['WH-BK-4']
+        exp = [('WH', 'BK', '4')]
+        out = self.parser.parse(token)
+        assert exp == out
+
+    def test_list_parser_double_nested(self, list_parser):
+        new_parser = TextParser.ListParser(self.parser)
+        token = [['WH-BK-4', '_'], ['1', '4-5'], []]
+        exp = [[('WH', 'BK', '4'), tuple()], [('1',), ('4', '5')], []]
+        out = new_parser.parse(token)
+        assert exp == out
+
+    @pytest.fixture
+    def dict_parser(self):
+        sub_parser = TextParser.BaseParser()
+        self.parser = TextParser.DictParser(sub_parser)
+
+    def test_dict_parser_1(self, dict_parser):
+        tokens = []
+        exp = {}
+        out = self.parser.parse(tokens)
+        assert exp == out
+    
+    def test_dict_parser_2(self, dict_parser):
+        tokens = [['five', '5'], ['six', '6'], ['8', 'ate']]
+        exp = {'five': 5, 'six': 6, '8': 'ate'}
+        out = self.parser.parse(tokens)
+        assert exp == out
+    
+    def test_dict_parser_3(self, dict_parser):
+        tokens = ['-nums', ['six', '6'], ['8', 'ate']]
+        exp = {'type': 'nums', 'six': 6, '8': 'ate'}
+        out = self.parser.parse(tokens)
+        assert exp == out
+    
+    def test_dict_parser_4(self, dict_parser):
+        tokens = ['-nums', ['six', '2', '4'], ['8', 'ate']]
+        exp = {'type': 'nums', 'six': [2, 4], '8': 'ate'}
+        out = self.parser.parse(tokens)
+        assert exp == out
+
+    def test_dict_parser_resets(self, dict_parser):
+        tokens = ['-nums', ['six', '6'], ['8', 'ate']]
+        exp = {'type': 'nums', 'six': 6, '8': 'ate'}
+        out = self.parser.parse(tokens)
+        assert exp == out
+        tokens_2 = ['-game', ['1', '4'], ['3', '4']]
+        exp_2 = {'type': 'game', '1': 4, '3': 4}
+        out_2 = self.parser.parse(tokens_2)
+        assert exp_2 == out_2
+    
+    def test_text_to_dict_1(self):
+        text = load_from_file('test_1.txt')
+        output = TextParser.parse_txt(text)
+        assert isinstance(output, list) # should be list of two puzzles
+        assert len(output) == 2
+        exp_1 = {'rules': [], 
+                 'grid': {'type': 'RectGrid',
+                          'height': 3, 'width': 3},
+                 'vertices': {'data': [tuple(), tuple(), ('3',),
+                              tuple(), tuple(), tuple(),
+                              ('1', ), tuple(), ('WH',)],
+                              'type': 'RectVertex',
+                              'encoding': 'full'}}
+        assert output[0] == exp_1
+
+    def test_text_to_dict_2(self):
+        text = load_from_file('test_1.txt')
+        output = TextParser.parse_txt(text)
+        exp_2 = {'rules': [{'type': 'Nurikabe'},
+                           {'type': 'Sudoku', 'reg_height': 3, 'reg_width': 2}], 
+                 'grid': {'type': 'RectGrid',
+                          'height': 3, 'width': 4},
+                 'vertices': {'data': [tuple(), ('WH', 'BK'), tuple(), tuple(),
+                              tuple(), ('3', '1', '0'), ('1', '5'), ('1',),
+                              ('BK',), tuple(), ('BK',), ('BK',)],
+                              'type': 'RectVertex',
+                              'encoding': 'full'}}
+        assert output[1] == exp_2
+
+
+      
+
+
 class Test_Puzzle_Construct():
     """
     Tests the constructor for both text and json formats.
     """
+
